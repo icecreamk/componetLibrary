876ee121f624f1362c454c741f78fdb8d4deae8b
llkkxx <550893685@qq.com>
2020-06-14 10:35:15 +0800

    button 组件

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 - 增加学习成本
 - 短期内增加开发成本
 - 长期维护的项目利于维护
+
+### classnames 维护类名
+> npm install classnames --save
+
+#### 支持typescript
+> npm install @types/classnames --save
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonSize, ButtonType} from './components/Button/button'
 
 function App() {
-  const a = '123'
-  if (a == '123') {
-
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button disabled>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">baidu link</Button>
       </header>
     </div>
   );
