36b34652a5e7888f5a61f8bf8ab63f455ade9797
llkkxx <550893685@qq.com>
2020-06-20 11:48:56 +0800

    menu

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import Button from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" />
+      <div>
         <p>类型</p>
-        <Button onClick={() => {console.log(1)}} btnType={'primary'}>Primary</Button>
-        <Button className="test" btnType={'danger'}>Danger</Button>
-        <Button btnType={'link'} href="http://www.baidu.com" target="_blank">Link</Button>
-        <Button btnType={'default'}>Default</Button>
-        <p>大小</p>
-        <Button btnType={'primary'} size={'lg'}>Primary</Button>
-        <Button btnType={'primary'} size={'sm'}>Primary</Button>
-      </header>
+          <Button onClick={() => {console.log(1)}} btnType={'primary'}>Primary</Button>
+          <Button className="test" btnType={'danger'}>Danger</Button>
+          <Button btnType={'link'} href="http://www.baidu.com" target="_blank">Link</Button>
+          <Button btnType={'default'}>Default</Button>
+          <p>大小</p>
+          <Button btnType={'primary'} size={'lg'}>Primary</Button>
+          <Button btnType={'primary'} size={'sm'}>Primary</Button>
+      </div>
+      <div>
+        <Menu defaultIndex={'0'}>
+          <MenuItem index={'0'}>
+            1
+          </MenuItem>
+          <MenuItem index={'1'}>
+            2
+          </MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/components/Menu/menu.tsx
@@ -0,0 +1,72 @@
+import React, { FC, useState, createContext, CSSProperties, cloneElement } from 'react'
+import classNames from 'classnames'
+import { MenuItemProps } from './menuItem'
+
+type MenuMode = 'horizontal' | 'vertical'
+
+export interface MenuProps {
+  defaultIndex?: string; /**默认 active 的菜单项的索引值 */
+  className?: string;
+  mode?: MenuMode; /**菜单类型 横向或者纵向 */
+  style?: CSSProperties;
+  onSelect?: (selectedIndex: string) => void;
+  defaultOpenSubMenus?: string[]; /**设置子菜单的默认打开 只在纵向模式下生效 */
+}
+
+interface IMenuContext {
+  index: string;
+  onSelect?: (selectedIndex: string) => void;
+  mode?: MenuMode;
+  defaultOpenSubMenus?: string[];  
+}
+
+export const MenuContext = createContext<IMenuContext>({index: '0'})
+
+export const Menu: FC<MenuProps> = (props) => {
+  const { className, mode, style, children, defaultIndex, onSelect, defaultOpenSubMenus } = props
+  const [ currentActive, setActive ] = useState(defaultIndex)
+  const classes = classNames('viking-menu', className, {
+    'menu-vertical': mode === 'vertical',
+    'menu-horizontal': mode !== 'vertical',
+  })
+  const handleClick = (index: string) => {
+    setActive(index)
+    if(onSelect) {
+      onSelect(index)
+    }
+  }
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : '0',
+    onSelect: handleClick,
+    mode,
+    defaultOpenSubMenus,
+  }
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<MenuItemProps>
+      const { displayName } = childElement.type
+      if (displayName === 'MenuItem' || displayName === 'SubMenu') {
+        // return React.cloneElement(childElement, {
+        //   index: index.toString()
+        // })
+        return childElement
+      } else {
+        console.error("Warning: Menu has a child which is not a MenuItem component")
+      }
+    })
+  }
+  return (
+    <ul className={classes} style={style} data-testid="test-menu">
+      <MenuContext.Provider value={passedContext}>
+        {renderChildren()}
+      </MenuContext.Provider>
+    </ul>
+  )
+}
+Menu.defaultProps = {
+  defaultIndex: '0',
+  mode: 'horizontal',
+  defaultOpenSubMenus: [],
+}
+
+export default Menu;
