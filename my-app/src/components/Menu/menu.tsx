133a35943772c1952a69b542f2de40e76db20e7e
llkkxx <550893685@qq.com>
2020-07-25 21:52:12 +0800

    Menu 添加storybook

--- my-app/src/components/Menu/menu.tsx
+++ my-app/src/components/Menu/menu.tsx
@@ -3,16 +3,18 @@ import classNames from 'classnames'
 import { MenuItemProps } from './menuItem'
 
 type MenuMode = 'horizontal' | 'vertical'
-
 export interface MenuProps {
-  defaultIndex?: string; /**默认 active 的菜单项的索引值 */
+  /**默认 active 的菜单项的索引值 */
+  defaultIndex?: string;
   className?: string;
-  mode?: MenuMode; /**菜单类型 横向或者纵向 */
+  /**菜单类型 横向或者纵向 */
+  mode?: MenuMode;
   style?: CSSProperties;
+  /**点击菜单项触发的回掉函数 */
   onSelect?: (selectedIndex: string) => void;
-  defaultOpenSubMenus?: string[]; /**设置子菜单的默认打开 只在纵向模式下生效 */
+  /**设置子菜单的默认打开 只在纵向模式下生效 */
+  defaultOpenSubMenus?: string[];
 }
-
 interface IMenuContext {
   index: string;
   onSelect?: (selectedIndex: string) => void;
@@ -21,6 +23,12 @@ interface IMenuContext {
 }
 
 export const MenuContext = createContext<IMenuContext>({index: '0'})
+/**
+ * 为网站提供导航功能的菜单。支持横向纵向两种模式，支持下拉菜单。
+ * ~~~js
+ * import { Menu } from ''
+ * ~~~
+ */
 export const Menu: FC<MenuProps> = (props) => {
   const { className, mode, style, children, defaultIndex, onSelect, defaultOpenSubMenus } = props
   const [ currentActive, setActive ] = useState(defaultIndex)
