907c629f278e1f11375b26c1978d65e689fe0652
llkkxx <550893685@qq.com>
2020-08-02 09:45:26 +0800

    hooks untils

---
+++ my-app/src/hooks/useDebounce.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+// 自定义hooks
+function useDebounce(value: any, delay = 300) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  useEffect(() => {
+    const handler = window.setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+  return debouncedValue
+}
+
+export default useDebounce;
